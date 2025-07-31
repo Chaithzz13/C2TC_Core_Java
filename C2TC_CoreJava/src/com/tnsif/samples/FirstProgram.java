46f9aafd6d670a0092970a55532bae3147a9a87b
Chaithzz13 <142679811+Chaithzz13@users.noreply.github.com>
2025-07-31 10:30:34 +0530

    Initial Commit

---
+++ C2TC_CoreJava/src/com/tnsif/samples/FirstProgram.java
@@ -0,0 +1,14 @@
+package com.tnsif.samples;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("Welcome to java learning");
+		System.out.println("Welcome to java learning");
+		System.out.println("Welcome to java learning");
+		// TODO Auto-generated method stub
+
+	}
+
+}
